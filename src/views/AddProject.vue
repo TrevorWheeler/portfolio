731f220ea248af7565ac0529f85e9f993ba91df0
Trevor Wheeler <sendtrevmail@gmail.com>
2019-02-27 10:21:27 +1000

    add client upload to cloudinary

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="add-project-page">
-
+    <input
+      type="file"
+      class="form-control"
+      v-on:change="upload($event.target.files)"
+      accept="image/*"
+    />
   </div>
 </template>
 
@@ -9,10 +14,41 @@ import axios from "axios";
 export default {
   name: "add-project",
   data() {
-    return {};
+    return {
+      cloudinary: {
+        uploadPreset: "",
+        apiKey: "",
+        cloudName: ""
+      },
+
+      thumbs: []
+    };
   },
-  computed: {},
-  methods: {}
+  computed: {
+    clUrl: function() {
+      return `https://api.cloudinary.com/v1_1/${
+        this.cloudinary.cloudName
+      }/upload`;
+    }
+  },
+  methods: {
+    upload: function(file) {
+      const formData = new FormData();
+      formData.append("file", file[0]);
+      formData.append("upload_preset", this.cloudinary.uploadPreset);
+      formData.append("tags", "gs-vue,gs-vue-uploaded");
+      // For debug purpose only
+      // Inspects the content of formData
+      for (var pair of formData.entries()) {
+        console.log(pair[0] + ", " + pair[1]);
+      }
+      axios.post(this.clUrl, formData).then(res => {
+        this.thumbs.unshift({
+          url: res.data.secure_url
+        });
+      });
+    }
+  }
 };
 </script>
 
