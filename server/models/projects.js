923efb66b4212082690270940452534a490585a5
Trevor Wheeler <sendtrevmail@gmail.com>
2019-02-27 10:17:26 +1000

    add new project page

--- server/models/projects.js
+++ server/models/projects.js
@@ -1,47 +1,135 @@
-const mongoose = require("mongoose");
-const User = require("./user");
+const mongoose = require('mongoose');
+const User = require('./user');
 
 const projectsSchema = new mongoose.Schema(
-  {
-    commercial: [
-      {
-        name: {
-          type: String
-        },
-        description: {
-          type: String
-        },
-        image: {
-          type: String
-        },
-        link: {
-          type: String
-        },
-        repo: {
-          type: String
-        }
-      }
-    ],
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }
-  },
-  {
-    timestamps: true
-  }
+	{
+		commercial: [
+			{
+				name: {
+					type: String
+				},
+				description: {
+					type: String
+				},
+				image: {
+					type: String
+				},
+				link: {
+					type: String
+				},
+				repo: {
+					type: String
+				}
+			}
+		],
+		fullStack: [
+			{
+				name: {
+					type: String
+				},
+				description: {
+					type: String
+				},
+				image: {
+					type: String
+				},
+				link: {
+					type: String
+				},
+				repo: {
+					type: String
+				}
+			}
+		],
+		frontEnd: [
+			{
+				name: {
+					type: String
+				},
+				description: {
+					type: String
+				},
+				image: {
+					type: String
+				},
+				link: {
+					type: String
+				},
+				repo: {
+					type: String
+				}
+			}
+		],
+		backEnd: [
+			{
+				name: {
+					type: String
+				},
+				description: {
+					type: String
+				},
+				image: {
+					type: String
+				},
+				link: {
+					type: String
+				},
+				repo: {
+					type: String
+				}
+			}
+		],
+		tags: [
+			{
+				one: {
+					type: String
+				},
+				two: {
+					type: String
+				},
+				three: {
+					type: String
+				},
+				four: {
+					type: String
+				},
+				five: {
+					type: String
+				},
+				six: {
+					type: String
+				},
+				seven: {
+					type: String
+				},
+				eight: {
+					type: String
+				},
+				nine: {
+					type: String
+				}
+			}
+		],
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	},
+	{
+		timestamps: true
+	}
 );
 
-projectsSchema.pre("remove", async function(next) {
-  try {
-    let user = await User.findById(this.user);
-    user.projects.remove(this.id);
-    await user.save();
-  } catch (err) {
-    return next(err);
-  }
+projectsSchema.pre('remove', async function(next) {
+	try {
+		let user = await User.findById(this.user);
+		user.projects.remove(this.id);
+		await user.save();
+	} catch (err) {
+		return next(err);
+	}
 });
 
-const Projects = mongoose.model("Projects", projectsSchema);
+const Projects = mongoose.model('Projects', projectsSchema);
 
 module.exports = Projects;
---
+++ src/views/AddProject.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="add-project-page">
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "add-project",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.add-project-page {
+  padding: 500px;
+}
+</style>
+
