1c3b4d61ea652f1bd77c87be062aac048a5ccace
Trevor Wheeler <sendtrevmail@gmail.com>
2019-03-14 08:34:06 +1000

    change to auth route on server

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = async function(req, res, next) {
+exports.signin = async function (req, res, next) {
   try {
     let user = await db.User.findOne({
       username: req.body.username
@@ -35,29 +35,4 @@ exports.signin = async function(req, res, next) {
   }
 };
 
-exports.signup = async function(req, res, next) {
-  try {
-    let user = await db.User.create(req.body);
-    let { id, username } = user;
-    let token = jwt.sign(
-      {
-        id,
-        username
-      },
-      process.env.SECRET_KEY
-    );
-    return res.status(200).json({
-      id,
-      username,
-      token
-    });
-  } catch (err) {
-    if (err.code === 11000) {
-      err.message = "Sorry, that username and/or email is taken";
-    }
-    return next({
-      status: 400,
-      message: err.message
-    });
-  }
-};
+
