264fa0a167c6429ddce3da9359f3969bdb2f1d46
Trevor Wheeler <sendtrevmail@gmail.com>
2024-09-28 16:05:31 +1000

    Card and button redesign

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -43,7 +43,6 @@ function scrollToProjects(): void {
 <style lang="scss">
 #hero-section {
   height: calc(100vh - 70px);
-  background-color: var(--secondary);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -147,12 +146,25 @@ function scrollToProjects(): void {
     color: var(--primary);
     border: 1px solid var(--primary);
     transition: 0.3s ease-in-out;
+  }
 
-    &:hover {
-      box-shadow: 0px 0px 0px 4px var(--secondary) inset,
-        0px 0px 0px 6px var(--accent) inset;
-      color: var(--text-light);
-    }
+  .hero-link-contact::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: 3px solid var(--accent);
+    border-radius: 2px;
+    opacity: 0;
+    transform: scale(1);
+    transition: all 0.3s ease-in;
+  }
+
+  .hero-link-contact:hover::before {
+    opacity: 1;
+    transform: scaleY(0.85) scaleX(0.95);
   }
 
   .chevron-down {
--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -56,8 +56,8 @@ const projects: ComputedRef<Project[]> = computed(() => {
   return [
     {
       name: "GoDesta",
-      description: "During my five years of employment at GoDesta, I played a pivotal role in the development and enhancement of a comprehensive transport logistics system, guiding the project from its inception through to the successful onboarding of our first client.I worked on both frontend and backend using TypeScript and Java to create a robust, scalable platform. I implemented critical features for job booking, tracking, and integrated accounting and invoicing, while managing complex third- party integrations. My role combined technical expertise and project management, delivering high - quality solutions that improved operational efficiency and client engagement.",
-      tags: ["Professional Project", "Apache Kafka", "Java", "Spring Framework", "TypeScript", "Node.js", "JavaScript", "Vue.js", "HTML", "CSS", "webpack", "MongoDB", "Redis", "Jasper Reports", "Email", "TeamCity", "Docker", "Rancher", "Atlassian products"],
+      description: "During my five years at GoDesta, I played a key role in developing and enhancing a comprehensive transport logistics platform, guiding the project from inception to client onboarding. I worked across both the frontend and backend, building a robust, scalable system. I implemented essential features, including job booking, real-time tracking, and integrated accounting and invoicing, while managing complex third-party integrations. In addition to my technical responsibilities, I handled aspects of project management, delivering high-quality solutions that significantly improved operational efficiency and client engagement.",
+      tags: ["Apache Kafka", "Java", "Spring Framework", "TypeScript", "Node.js", "JavaScript", "Vue.js", "HTML", "CSS", "webpack", "MongoDB", "Redis", "Jasper Reports", "Email", "TeamCity", "Docker", "Rancher", "Atlassian products"],
       image: "GoDesta.webp",
       repo: "",
       link: "https://godesta.com.au/",
@@ -67,7 +67,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Trevy",
       description: "Trevy is a personal passion project aimed at creating a comprehensive business management solution to streamline operations across various service industries. Currently in early development, Trevy features a landing page, user sign-up, dashboard, client management, and interactive maps powered by Mapbox. This project highlights my expertise in modern web development and my commitment to delivering high-quality, maintainable, and visually appealing applications.",
       image: "trevy.webp",
-      tags: ["Personal Project", "HTML5", "CSS3", "Vite", "Vue.js", "Go", "Echo", "AWS SES", "MongoDB"],
+      tags: ["HTML5", "CSS3", "Vite", "Vue.js", "Go", "Echo", "AWS SES", "MongoDB"],
       repo: "",
       link: "",
       type: PROJECT_CATEGORY.COMMERCIAL
@@ -76,7 +76,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Discord Jukebox",
       description: "Discord Jukebox is a feature-rich Discord bot enhancing communal music experiences. Developed before Spotify Sessions, it allows users to queue and manage songs from various sources, including YouTube, directly within Discord. The bot pipes audio through the microphone input, enabling real-time listening for all users. With commands for playlist management and additional fun features, Discord Jukebox showcases skills in bot development, API integration, and creating engaging community tools, though it's now largely deprecated due to Spotify's introduction of shared listening features.",
       image: "filthy-bot.webp",
-      tags: ["Personal Project", "Bot Application", "Node.js", "Typescript", "Puppeteer", "Discord.js", "FFmpeg"],
+      tags: ["Bot Application", "Node.js", "Typescript", "Puppeteer", "Discord.js", "FFmpeg"],
       repo: "https://github.com/TrevorWheeler/filthy-bot",
       link: "",
       type: PROJECT_CATEGORY.BOT_APPLICATION
@@ -85,7 +85,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Koala Cuts",
       description: "Koala Cuts is a fictional business website created as a fun project to explore and showcase my web development and design skills. Designed for an imaginary landscaping service, the site offers a clear and inviting overview of various services to potential clients. This project focused on experimenting with Lerna and monorepos to efficiently manage and scale multiple packages within a single codebase. I also created all vector designs and the logo using Figma, demonstrating my ability to handle both development and design aspects to build robust, maintainable, and visually appealing web applications.",
       image: "koala-cuts.webp",
-      tags: ["Personal Project", "Monorepo", "Lerna", "Node.js", "Typescript", "Vue.js", "Fastify", "HTML5", "CSS3", "Figma"],
+      tags: ["Monorepo", "Lerna", "Node.js", "TypeScript", "Vue.js", "Fastify", "HTML5", "CSS3", "Figma"],
       repo: "https://github.com/TrevorWheeler/koala-cuts",
       link: "",
       type: PROJECT_CATEGORY.COMMERCIAL
@@ -94,7 +94,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Forum",
       description: "Forum is a dynamic project designed to quickly spin up new forums as needed. The platform supports both anonymous users and account creation, providing flexibility in user engagement. Posts on the forum are short-lived, ensuring that the content remains fresh and the website can adapt to changing discussions. Drawing inspiration from various famous forum and social media websites, I aimed to create an intuitive and user-friendly experience. This project demonstrates my ability to integrate features from different platforms, innovate on traditional forum structures, and develop a versatile and responsive web application.",
       image: "forum-generator.webp",
-      tags: ["Personal Project", "Typescript", "Vue.js", "Node.js", "Express.js", "HTML", "CSS"],
+      tags: ["TypeScript", "Vue.js", "Node.js", "Express.js", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/forum-server",
       link: "",
       type: PROJECT_CATEGORY.FULL_STACK
@@ -104,7 +104,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "ShotBall",
       description: "Shot Ball is a 2D multiplayer online space shooting game developed in Go. The game features real-time combat in a dynamic space environment, where players can compete against each other using a click to shoot mechanic. Developing Shot Ball involved leveraging the Go programming language to create a responsive and engaging multiplayer experience. This project demonstrates my skills in game development, network programming, and real-time interaction, showcasing the ability to create complex and entertaining applications.",
       image: "shotball.webp",
-      tags: ["Personal Project", "Online Multiplayer", "Typescript", "Vue.js", "Node.js", "Express.js", "HTML", "CSS"],
+      tags: ["TypeScript", "Vue.js", "Node.js", "Express.js", "HTML", "CSS", "Online Multiplayer"],
       repo: "https://github.com/TrevorWheeler/shotball-server",
       link: "",
       type: PROJECT_CATEGORY.FULL_STACK
@@ -113,7 +113,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "The Caf",
       description: "I was tasked with updating an existing WordPress website for The Caf, a vibrant coffee shop in Coolum Beach. The goal was to create a dynamic and engaging online presence that showcased the café's events and menu. Throughout the project, I gained deeper expertise in WordPress and web design. The client was very pleased with the outcome, noting a significant improvement over their outdated previous website.",
       image: "thecaf.webp",
-      tags: ["Professional Project", "jQuery", "Wordpress", "HTML5", "CSS3", "Bootstrap", "Figma"],
+      tags: ["jQuery", "Wordpress", "HTML5", "CSS3", "Bootstrap", "Figma"],
       repo: "",
       link: "",
       type: PROJECT_CATEGORY.COMMERCIAL
@@ -121,7 +121,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
     {
       name: "Sandy Pages Books",
       description: "I was tasked to upgrade their existing website with a more modern approach. An E-commerce solution was required as Sandy Pages books partnered with local schools to provide an outlet for students to purchase books during the schools book fest week.",
-      tags: ["Professional Project", "Wordpress", "Woo-commerce", "cPanel", "HTML5", "CSS3", "Bootstrap", "jQuery", "Email"],
+      tags: ["Wordpress", "Woo-commerce", "cPanel", "HTML5", "CSS3", "Bootstrap", "jQuery", "Email"],
       image: "sandypagesbooks.webp",
       repo: "",
       link: "",
@@ -130,7 +130,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
     {
       name: "North Shore Realty",
       description: "I was tasked with creating professional and cohesive email signatures for North Shore Realty, a real estate company in Coolum Beach. Utilizing HTML and CSS, I designed email signatures that were not only visually appealing but also cross-compatible with major email providers such as Outlook and Gmail. Ensuring consistency across the organization, the signatures were easily applied to their Outlook email clients. This project involved meticulous attention to detail to guarantee that the design rendered correctly across different platforms, enhancing the company's branding and communication efforts.",
-      tags: ["Professional Project", "HTML", "CSS", "Email"],
+      tags: ["HTML", "CSS", "Email"],
       image: "northshorerealty.webp",
       repo: "",
       link: "",
@@ -140,7 +140,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "HTML5 Video Player",
       description: "I developed a lightweight, customizable HTML5 video player using pure HTML and JavaScript, featuring controls for play, pause, volume, and fullscreen. This project showcases my proficiency in HTML5 and JavaScript, and my ability to create standalone web components for website integration.",
       image: "html5-video-player.webp",
-      tags: ["Personal Project", "Javascript", "HTML", "CSS"],
+      tags: ["Javascript", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/html5-video-player",
       link: "",
       type: PROJECT_CATEGORY.FRONT_END
@@ -158,7 +158,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Guess The Colour",
       description: "Guess the Colour is a learning project I developed to enhance my understanding of RGB color values and improve my programming skills. The game presents users with RGB values and challenges them to guess the correct color from a set of options. This project was instrumental in reinforcing my knowledge of color theory and web development fundamentals.",
       image: "guess-the-colour.webp",
-      tags: ["Personal Project", "Javascript", "HTML", "CSS"],
+      tags: ["Javascript", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/guess-the-colour",
       link: "",
       type: PROJECT_CATEGORY.FRONT_END
@@ -167,7 +167,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Score Keeper",
       description: "Score-Keeper is an application designed to help users easily keep track of scores in turn-based games. The app provides a simple and intuitive interface for recording and managing scores, ensuring that players can focus on the game without worrying about keeping accurate tallies. Developed to enhance my skills in app design and user experience, this project showcases my ability to create practical and user-friendly applications.",
       image: "score-keeper.webp",
-      tags: ["Personal Project", "Javascript", "HTML", "CSS"],
+      tags: ["Javascript", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/score-keeper",
       link: "",
       type: PROJECT_CATEGORY.FRONT_END
@@ -176,7 +176,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Kerbside City",
       description: "Kerbside City is an application I developed to simplify the process of finding kerbside pickup dates for residents in Brisbane. Frustrated with the user experience on the council website, I created an intuitive platform where users can simply type in their suburb and instantly receive the relevant pickup dates. The application crawls the council website to gather the necessary information and presents it in a user-friendly format. This project not only addressed a common pain point for residents but also showcases my skills in web scraping, data presentation, and improving user experience.",
       image: "kerbside-city.webp",
-      tags: ["Personal Project", "SSR", "Nuxt.js", "Typescript", "Node.js", "Express.js", "HTML", "CSS"],
+      tags: ["SSR", "Nuxt.js", "TypeScript", "Node.js", "Express.js", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/kerbside-city",
       link: "",
       type: PROJECT_CATEGORY.FULL_STACK
@@ -185,7 +185,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Float Out",
       description: "Float Out is an application I developed to streamline cash handling tasks during my time working in hospitality. The app calculates the difference between the float amount and profits made in a straightforward, linear manner. Instead of manually adding up each denomination of coins and notes, users can simply input the total number of each denomination, and the app automatically tallies the amounts. This tool significantly simplifies the process of reconciling the cash drawer, allowing users to quickly determine the amount to take out of the float. 'Float Out' highlights my ability to identify practical problems in everyday tasks and develop efficient, user-friendly solutions to address them.",
       image: "floatout.webp",
-      tags: ["Personal Project", "Javascript", "HTML", "CSS"],
+      tags: ["Javascript", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/float-out",
       link: "",
       type: PROJECT_CATEGORY.FRONT_END
@@ -195,7 +195,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "CRUD Boilerplate",
       description: "CRUD Boilerplate is a deprecated starter template for backend Node.js applications, designed to streamline the development process. This boilerplate includes built-in JWT authentication for secure user management and a simple projects route to demonstrate basic CRUD operations. The project served as a foundational template for quickly setting up new backend applications, ensuring consistent structure and best practices. Although it is no longer actively maintained, CRUD Boilerplate highlights my proficiency in Node.js, authentication mechanisms, and creating reusable code for efficient project initialization.",
       image: "CRUDapi.webp",
-      tags: ["Personal Project", "Javascript", "Node.js", "Express.js", "HTML", "CSS"],
+      tags: ["Javascript", "Node.js", "Express.js", "HTML", "CSS"],
       repo: "https://github.com/TrevorWheeler/express-mongodb-crud-jwt",
       link: "",
       type: PROJECT_CATEGORY.BACK_END
@@ -270,7 +270,6 @@ const projects: ComputedRef<Project[]> = computed(() => {
     }
 
     .card {
-      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.4), 0 1px 2px rgba(0, 0, 0, 0.24);
       width: 100%;
       margin: 0 0 7em 0;
       display: flex;
@@ -278,8 +277,14 @@ const projects: ComputedRef<Project[]> = computed(() => {
       background-color: var(--primary);
       position: relative;
       justify-content: space-between;
-      border-radius: 3px;
+      border-radius: 4px;
       overflow: hidden;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3), 0 1px 2px rgba(0, 0, 0, 0.2);
+      transition: box-shadow 0.3s ease-in-out;
+
+      &:hover {
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.4), 0 1px 2px rgba(0, 0, 0, 0.24);
+      }
 
       @media only screen and (min-width: 768px) {
         width: calc(50% - 0.5em);
@@ -322,8 +327,12 @@ const projects: ComputedRef<Project[]> = computed(() => {
         text-align: center;
         background-color: var(--secondary);
         margin: 0;
-        line-height: 72px;
+        line-height: 60px;
         color: var(--text-light);
+        background-image: radial-gradient(circle at 2px 2px,
+            rgba(255, 255, 255, 0.05) 1px,
+            transparent 0);
+        background-size: 20px 20px;
       }
 
       .project-img {
@@ -335,24 +344,25 @@ const projects: ComputedRef<Project[]> = computed(() => {
       }
 
       .project-description {
-        margin: 1.4em 0;
-        padding: 0 20px;
+
+        padding: 16px;
         font-size: 1.8em;
         font-weight: 400;
       }
 
       .project-tag-container {
-        padding: 0 20px 25px;
+        padding: 0 16px 16px 16px;
 
         .project-tag {
-          border: 2px solid var(--accent);
+          border: 1px solid var(--secondary);
           display: inline-flex;
-          margin: 0 1em 1em 0;
-          border-radius: 2px;
+          padding: 6px 8px 4px;
+          margin: 0 4px 12px 0;
+          border-radius: 4px;
 
           span {
             font-size: 1.45em;
-            padding: 0.5em;
+
             font-weight: 600;
             line-height: 1.3rem;
           }
@@ -364,6 +374,10 @@ const projects: ComputedRef<Project[]> = computed(() => {
         justify-content: space-between;
         padding: 2em 20px;
         background-color: var(--secondary);
+        background-image: radial-gradient(circle at 2px 2px,
+            rgba(255, 255, 255, 0.05) 1px,
+            transparent 0);
+        background-size: 20px 20px;
 
         .project-link {
           color: var(--accent);
