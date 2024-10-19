8b59867b1e3e280526be9864d4eeeb8cff60b05f
Trevor Wheeler <sendtrevmail@gmail.com>
2024-10-19 15:41:20 +1000

    update project image to latest version

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -67,7 +67,7 @@ const projects: ComputedRef<Project[]> = computed(() => {
       name: "Trevy",
       description: "Trevy is a personal passion project aimed at creating a comprehensive business management solution to streamline operations across various service industries. Currently in early development, Trevy features a landing page, user sign-up, dashboard, client management, and interactive maps powered by Mapbox. This project highlights my expertise in modern web development and my commitment to delivering high-quality, maintainable, and visually appealing applications.",
       image: "trevy.webp",
-      tags: ["HTML5", "CSS3", "Vite", "Vue.js", "Go", "Echo", "AWS SES", "MongoDB"],
+      tags: ["HTML5", "CSS3", "Vite", "Vue.js", "Go", "Echo", "AWS SES", "AWS S3", "Dart", "Flutter", "MongoDB"],
       repo: "",
       link: "",
       type: PROJECT_CATEGORY.COMMERCIAL
